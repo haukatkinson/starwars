0ae9c3efef309b336874116a7481a154b593ce41
haukatkinson <haukatkinson@aol.com>
2022-07-09 14:59:05 +0000

    routes for people, planets, and starships anded alon with searlizing them

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,86 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class People(db.Model):
+    __tablename__ = "people"
+    name = db.Column(db.String(256))
+    height = db.Column(db.Float)
+    mass = db.Column(db.String(256))
+    hair_color = db.Column(db.String(256))
+    skin_color = db.Column(db.String(256))
+    eye_color = db.Column(db.String(256))
+    birth_year = db.Column(db.String(256))
+    gender = db.Column(db.String(256))
+    homeworld = db.Column(db.String(256))
+    id = db.Column(db.Integer,unique=True, primary_key=True)
+
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "height": self.height,
+            "mass": self.mass,
+            "skin_color": self.skin_color,
+            "homeworld": self.homeworld
+        }
+
+
+class Planets(db.Model):
+    __tablename__ = "planets"
+    name = db.Column(db.String(256))
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+    climate = db.Column(db.String(256))
+    gravity = db.Column(db.Integer)
+    terrain = db.Column(db.String(256))
+    surface_water = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    id = db.Column(db.Integer,unique=True, primary_key=True)
+
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "population": self.population,
+        }
+
+
+class Starships(db. Model):
+    __tablename__ = "starships"
+    name = db.Column(db.String(256))
+    model = db.Column(db.String(256))
+    manufacturer = db.Column(db.String(256))
+    cost_in_credits = db.Column(db.Integer)
+    length = db.Column(db.Integer)
+    max_atmosphering_speed = db.Column(db.Integer)
+    passengers = db.Column(db.Integer)
+    cargo_capacity = db.Column(db.Integer)
+    id = db.Column(db.Integer,unique=True, primary_key=True)
+
+    
+    def serialize(self):
+        return {
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+        }
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Planets, People, Starships
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -14,5 +14,69 @@ def handle_hello():
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
+
+
+@api.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    if people is None:
+        return jsonify(msg="This person does not exist"),400
+    else:
+        return jsonify(
+        data=[person.serialize() for person in people]),200
+
+@api.route('/people/<int:id>', methods=['GET'])
+def get_person(id):
+    people = People.query.filter(
+        people.id == id
+    ).one_or_none()
+    if people is None:
+        return jsonify(msg="This person does not exist"),400
+    else:
+        return jsonify(
+            data=person.serialize()
+        ),200
+
+@api.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    if planets is None:
+        return jsonify(msg="This person does not exist"),400
+    else:
+        return jsonify(
+        data=[planet.serialize() for planet in planets]),200
+
+@api.route('/planets/<int:id>', methods=['GET'])
+def get_planet(id):
+    planets = Planets.query.filter(
+        planets.id == id
+    ).one_or_none()
+    if planet is None:
+        return jsonify(msg="This person does not exist"),400
+    else:
+        return jsonify(
+            data=planet.serialize()
+        ),200
+
+@api.route('/starships', methods=['GET'])
+def get_starships():
+    starships = Starships.query.all()
+    if starships is None:
+        return jsonify(msg="This person does not exist"),400
+    else:
+        return jsonify(
+        data=[starship.serialize() for starship in starships]),200
+
+@api.route('/starships/<int:id>', methods=['GET'])
+def get_starship(id):
+    starships = Starships.query.filter(
+        starships.id == id
+    ).one_or_none()
+    if starship is None:
+        return jsonify(msg="This person does not exist"),400
+    else:
+        return jsonify(
+            data=starship.serialize()
+        ),200
 
     return jsonify(response_body), 200
